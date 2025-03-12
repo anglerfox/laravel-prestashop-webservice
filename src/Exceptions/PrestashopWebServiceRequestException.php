8322f1ffe51fb55b3ef2193d01b25f63ee22a2e1
github-sakshi-jain <github+sakshi.jain@anglerfox.com>
2025-03-12 10:42:41 +0530

    Exception param update for redirect & status code

--- src/Exceptions/PrestashopWebServiceRequestException.php
+++ src/Exceptions/PrestashopWebServiceRequestException.php
@@ -5,10 +5,11 @@ namespace Protechstudio\PrestashopWebService\Exceptions;
 class PrestashopWebServiceRequestException extends PrestashopWebServiceException
 {
     protected $response;
+    protected $baseUrl;
 
-    public function __construct($message = null, $code = null, $response = null)
+    public function __construct($message = null, $code = null, $response = null, $baseUrl = null)
     {
-       if ($response instanceof \SimpleXMLElement && isset($response->errors->error->message)) {
+        if ($response instanceof \SimpleXMLElement && isset($response->errors->error->message)) {
             $message = (string) $response->errors->error->message;
         }
 
@@ -17,15 +18,30 @@ class PrestashopWebServiceRequestException extends PrestashopWebServiceException
         parent::__construct($finalMessage, $code);
 
         $this->response = $response;
+        $this->baseUrl = $baseUrl; // Store base URL
     }
 
+    /**
+     * Check if the response exists and is not empty
+     */
     public function hasResponse()
     {
-        return isset($this->response) && !empty($this->response);
+        return !empty($this->response);
     }
 
+    /**
+     * Get the response data
+     */
     public function getResponse()
     {
         return $this->response;
+    }
+
+    /**
+     * Get the Base URL
+     */
+    public function getBaseUrl()
+    {
+        return $this->baseUrl;
     }
 }
